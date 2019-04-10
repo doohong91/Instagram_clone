eb45bd4723d8f4d123a18c855f4cf9e1dab93d65
doohong91 <doohong91@gmail.com>
2019-04-10 04:17:58 +0000

    customize modelform

--- posts/forms.py
+++ posts/forms.py
@@ -2,17 +2,16 @@ from django import forms
 from .models import Post
 
 class PostModelForm(forms.ModelForm):
-    content = forms.CharField(label='content')
+    content = forms.CharField(
+        label='content', 
+        widget=forms.Textarea(attrs={
+            'rows': '5',
+            'cols': '50',
+            'placeholder': '지금 뭘 하고 계신가요?',
+            })
+    )
     
     class Meta:
         model = Post
         # input을 만들 column을 list로 만들어 넣어줌.
         fields = ['content',]
-        widget = {
-            'content': forms.Textarea(attrs={
-                'class': '',
-                'rows': '5',
-                'cols': '50',
-                'placeholder': '지금 뭘 하고 계신가요?',
-            })
-        }
