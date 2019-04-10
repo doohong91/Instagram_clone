dfb57f32dd20c870a62aaa1d0924f276b9025855
doohong91 <doohong91@gmail.com>
2019-04-10 02:40:20 +0000

    admin page에서 정리된 리스트 볼 수 있도록 model.ptd에서 str메소드 오버라이드

--- posts/models.py
+++ posts/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    content = models.CharField(max_length=140)
+    content = models.CharField(max_length = 140)
+    
+    def __str__(self):
+        return '<{}: {}>'.format(self.id, self.content[:20])
