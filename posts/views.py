0238fdcac8b6ad9a2ff7fcec72aa3e9d1d67079a
doohong91 <doohong91@gmail.com>
2019-04-10 02:44:23 +0000

    django-bootstrap4 패키지 설치 & bootstrap4를 활용해 modelform에 bootstrap 적용

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PostModelForm
+
 
 # Create your views here.
 def create(request):
     # 'POST'요청이 오면,
     if request.method == 'POST':
         # 글을 작성하기
-        pass
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:create')
+        
     # 'GET'요청이 오면,
     else:
         # post를 작성하는 form을 가져와 template에서 보여줌. 
